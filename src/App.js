e50453f30e5b5bbfb8ac0e15bad89ee5f6e24478
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-12 11:00:04 -0500

    using useeffect to prevent api fetch loop

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import StarshipPage from "./components/StarshipPage";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
+
+  const [starships, setStarships] = useState([]);
+
+
+  useEffect(() => {
+    console.log("hi from use effect");
+
+    Axios.get(`https://swapi.dev/api/starships/`)
+    .then((res) => {
+      let starshipArray = res.data.results;
+      setStarships(starshipArray);
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
+
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Route exact path="/" component={StarshipPage} />
+        <Route 
+        exact 
+        path="/" 
+        render={() => <StarshipPage starships={starships} />} />
       </div>
     </BrowserRouter>
   );
